26cd6517ccfd4c8c6ab423366f770e6fb957cce9
Shwetas Dhake <shwetas.dhake@mitaoe.ac.in>
2024-08-18 02:21:46 +0530

    model updated

--- assistant.py
+++ assistant.py
@@ -67,7 +67,7 @@ async def entrypoint(ctx: JobContext):
         ]
     )
 
-    gpt = openai.LLM(model="GPT-4o mini")
+    gpt = openai.LLM(model="GPT-3.5 Turbo")
 
     # Since OpenAI does not support streaming TTS, we'll use it with a StreamAdapter
     # to make it compatible with the VoiceAssistant
